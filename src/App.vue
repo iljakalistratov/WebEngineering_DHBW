29ed3e0b11dba1a9bc66d43f5e6be2622ab3f043
iljakalistratov <71067944+iljakalistratov@users.noreply.github.com>
2021-12-04 23:42:56 +0100

    Outsourced Article int own Component

--- src/App.vue
+++ src/App.vue
@@ -12,31 +12,29 @@
 
       <div class="weatherbox">
         <div class="temperature">{{ Math.round(weather.main.temp) }}°C</div>
-        <div class="weather">{{ weather.weather[0].main }}</div>
+        <div class="weather"></div>
       </div>
     </div>
 
-    <div id="wrapper2" class="wiki-wrapper">
-      <t1>{{ weather.name }}</t1>
-      <div class="article">
-        {{ this.article }}
-      </div>
-      <a>
-          <img src="./assets/play.png" style="width:30px; height: auto;"/>
-      </a>
-    </div>
+    <Article ref="articleComponent"></Article>
+
+    
   </main>
 </template>
 
 <script>
-
+import Article from './components/Article.vue'
 export default {
   name: "App",
+
+  components: {
+    Article,
+  },
+
   data () {
     return{
       api_key: '1f3e379d7b4cbb2bbb0527cb6b3a654b',
-      url_base: 'https://api.openweathermap.org/data/2.5/',
-      url_base_wiki: 'https://de.wikipedia.org/w/api.php?action=query&titles=',  //&prop=extracts&format=json&exintro=1
+      url_base: 'https://api.openweathermap.org/data/2.5/',  //&prop=extracts&format=json&exintro=1
       query:'',
       weather: {}
     }
@@ -48,29 +46,16 @@ export default {
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
-        }).then(this.setWeather).then(this.getArticle);
-
+        }).then(this.setWeather).then(this.$refs.articleComponent.getArticle(this.query));
+        
         //this.getArticle();
       }
     },
 
-    getArticle () {
-      fetch(`${this.url_base_wiki}${this.query}&prop=extracts&format=json&exintro=1&origin=*`)
-      .then(art => {
-        return art.json();
-      }).then(this.setArticle);
-      },
-
-    sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-    },
 
     setWeather (results) {
       this.weather = results;
-    },
-
-    setArticle (result) {
-      this.article = result;
+      this.weatherStatus = this.weather.weather[0].main;
     },
 
     handleDate () {
@@ -208,16 +193,6 @@ main{
 }
 
 #wrapper1 {
-  display: inline-block;
-  padding: 10px 25px;
-  background-color: rgba(0 , 0, 0, 0.7);
-  border-radius: 20px;
-  margin: 30px 0px;
-
-  box-shadow: 3px 4px rgba(0, 0, 0, 0.3);
-}
-
-#wrapper2 {
   display: inline-block;
   padding: 10px 25px;
   background-color: rgba(0 , 0, 0, 0.7);
---
+++ src/components/Article.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="wrapper2" class="wiki-wrapper">
+      <t1></t1>
+      <div class="articleW"> {{ this.article }} </div>
+      <a>
+          <img src="../assets/play.png" style="width:30px; height: auto;"/>
+      </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Article',
+
+    methods: {
+        getArticle (ort) {
+      
+            const wtf = require('wtf_wikipedia')
+
+            wtf.fetch(ort, 'de').then((doc) => {
+                this.setArticle(doc.sections()[0].text());
+
+            })
+
+        this.$forceUpdate();
+        },
+
+        setArticle (result) {
+            this.article = result;
+            console.log(this.article);
+        },
+    }
+}
+</script>
+
+<style scoped>
+    
+    #wrapper2 {
+    display: inline-block;
+    padding: 10px 25px;
+    background-color: rgba(0 , 0, 0, 0.7);
+    border-radius: 20px;
+    margin: 30px 0px;
+
+    box-shadow: 3px 4px rgba(0, 0, 0, 0.3);
+    }
+
+</style>
